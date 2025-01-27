e61a5a02efd715be1b73a959b3c17a2dd2cc9986
piokalm <piokalm@gmail.com>
2025-01-27 12:18:06 +0100

    initial commit

--- backend/src/middleware/error.middleware.ts
+++
@@ -1,5 +0,0 @@
-import * as express from "express"
-
-export function handleAppErrors(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
-
-}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,7 @@
-import HomePage from "./home/page";
-import Header from "./components/Header";
+
 
 export default function Home() {
   return (
-    <main>
-      <Header></Header>
-      <HomePage></HomePage>
-    </main>
+    <main></main>
   );
 }
